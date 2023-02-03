402a83e7d3717069535659f3572016cd469f2291
Babak Ghaffari <100711307+Babak01Gh@users.noreply.github.com>
2023-02-03 23:56:49 +0330

    Add New Section (Products)

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ burgerMenu.addEventListener('click',()=>{
     ulMenu.classList.toggle('hasShown');
     serachBar.classList.toggle('hasShown')
 })
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.nextBtn',
+      prevEl: '.prevBtn',
+    },
+    breakpoints:{
+        1150:{
+            slidesPerView : 4
+        },
+        850:{
+            slidesPerView: 3
+        },
+        560:{
+            slidesPerView:2
+        },
+        else:{
+            slidesPerView:1
+        }
+    }
+  });
